eb10323ade0bd453c11f4af0ac83dbb64b4333fa
bharat <bharatpagare018@gmail.com>
2023-09-22 19:34:46 +0530

    this is commited.

---
+++ simple.py
@@ -0,0 +1,28 @@
+# this program for taking marks from number of student.
+"""list = []
+no_of_student = int(input("enter the number of student: "))
+for i in range(no_of_student):
+    marks = int(input("enter the marks: "))
+    list.append(marks)"""
+"""mark_list = []
+n = int(input("enter the number of student:"))
+for i in range(n):
+    m = int(input("enter the marks of student:"))
+    mark_list.append(m)"""
+
+empty_list = [] # create empty list
+n = int(input("enter the number of student: ")) # taking value of number of student.
+for i in range(n):# use for loop . in for loop we put the value of "n student"
+    marks_of_student = int(input("enter the student marks : "))# we taking marks from user.
+
+    empty_list.append(marks_of_student) # we append the mark of student to the empty list
+    # by using in-built function append
+
+
+# append function
+list = []
+b = int(input("enter the marks: "))
+list.append(b) # in-built function in python.   
+
+
+
